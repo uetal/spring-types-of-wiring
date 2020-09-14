d2bbeed74f0648438e26c23b60938198398f724d
Uetal <uetal@me.com>
2020-09-14 18:25:36 +0300

    14.09

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,18 @@
 package koschei.models;
 
+import koschei.KoscheiTheDeathless;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Needle7 {
+
+    private Deth8 deth;
+
+    @Autowired
+    public Needle7(Deth8 deth) {
+        this.deth = deth;
+    }
 
     @Override
     public String toString() {
